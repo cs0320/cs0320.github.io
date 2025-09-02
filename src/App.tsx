6bda711bcc1929115221c92e73a06bfc49121f79
Tim Nelson <tbnelson@gmail.com>
2025-09-02 12:26:58 -0400

    feat: checkbox to disable animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Footer from './components/Footer';
-
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [animationEnabled, setAnimationEnabled] = useState(() => {
+    const stored = localStorage.getItem('animationEnabled');
+    return stored === null ? true : stored === 'true';
+  });
+
+  useEffect(() => {
+    localStorage.setItem('animationEnabled', animationEnabled.toString());
+  }, [animationEnabled]);
+
   return (
     <>
-      <Navbar />
-      <Landing />
+      <Navbar animationEnabled={animationEnabled} setAnimationEnabled={setAnimationEnabled} />
+      <Landing animationEnabled={animationEnabled} />
     </>
-  );  
+  );
 }
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,56 @@
 import '../App.css';
 
-function Navbar() {
-  return (
-    <section className="navbar w-full px-8 text-gray-100 font-mono sticky top-0" style={{zIndex: 10000}}>
-    <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
-    {/* <img className="icon" src="/pumpkin.png"/> */}
-        <div className="relative flex flex-col md:flex-row">
-            {/* <span className="mx-auto thirtytwo leading-none ">cs0320</span> */}
-            <a href="/#" className="mx-auto thirtytwo leading-none">
-                cs0320
-            </a>
-        </div>
 
-        <div className="inline-flex items-center ml-5 space-x-10 lg:justify-end flex-wrap">
-            <a href="#course-info" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
-                Course Info
-            </a>
-            <a href="#assignments" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
-                Assignments
-            </a>
-            <a href="#gearups" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
-                Gear-Ups
-            </a>
-            <a href="#lectures" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
-                Lectures
-            </a>
-            <a href="#calendar" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
-                Hours
-            </a>
-            <a href="#forms" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
-                Forms
-            </a>
-            <a href="#staff" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
-                Staff
-            </a>
-        </div>
-    </div>
-</section>
+interface NavbarProps {
+    animationEnabled: boolean;
+    setAnimationEnabled: (enabled: boolean) => void;
+}
 
-  );  
+function Navbar({ animationEnabled, setAnimationEnabled }: NavbarProps) {
+    return (
+        <section className="navbar w-full px-8 text-gray-100 font-mono sticky top-0" style={{zIndex: 10000}}>
+            <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
+                <div className="relative flex flex-col md:flex-row">
+                    <a href="/#" className="mx-auto thirtytwo leading-none">
+                        cs0320
+                    </a>
+                </div>
+                <div className="inline-flex items-center ml-5 space-x-10 lg:justify-end flex-wrap">
+                    <a href="#course-info" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
+                        Course Info
+                    </a>
+                    <a href="#assignments" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
+                        Assignments
+                    </a>
+                    <a href="#gearups" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
+                        Gear-Ups
+                    </a>
+                    <a href="#lectures" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
+                        Lectures
+                    </a>
+                    <a href="#calendar" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
+                        Hours
+                    </a>
+                    <a href="#forms" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
+                        Forms
+                    </a>
+                    <a href="#staff" className="text-base font-medium leading-6 whitespace-no-wrap transition duration-150 ease-in-out ">
+                        Staff
+                    </a>
+                    {/* Animation toggle slider */}
+                    <label className="flex items-center space-x-2 ml-6 cursor-pointer select-none">
+                        <span className="text-sm">Animation</span>
+                        <input
+                            type="checkbox"
+                            checked={animationEnabled}
+                            onChange={e => setAnimationEnabled(e.target.checked)}
+                            className="form-checkbox h-5 w-5 text-blue-600"
+                        />
+                    </label>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Navbar;
